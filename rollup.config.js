bc6a09983752c308867a742692a44a25c993f836
yuanzeyu <yuanzeyu@onchain.com>
2021-09-02 11:28:50 +0800

    feat: update license

--- rollup.config.js
+++ rollup.config.js
@@ -8,14 +8,24 @@ import pkg from "./package.json";
 
 const moduleName = pkg.name.replace(/^@.*\//, "");
 const inputFileName = "src/index.ts";
-const { author } = pkg;
 const banner = `
   /**
-   * @license
-   * author: ${author}
-   * ${moduleName}.js v${pkg.version}
-   * Released under the ${pkg.license} license.
-   */
+    * Copyright (C) 2021 The ontology Authors
+    * This file is part of The ontology library.
+    *
+    * The ontology is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU Lesser General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * The ontology is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU Lesser General Public License for more details.
+    *
+    * You should have received a copy of the GNU Lesser General Public License
+    * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+    */
 `;
 
 export default [
